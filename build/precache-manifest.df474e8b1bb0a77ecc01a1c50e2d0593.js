5a640cc83aa3d5800779bff18dca874c5023c804
Alaili02 <52315374+Alaili02@users.noreply.github.com>
2020-06-04 12:45:47 +0300

    added build this time

---
+++ build/precache-manifest.df474e8b1bb0a77ecc01a1c50e2d0593.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f087164e7221f0aec27522ae9dbaef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfe67ba9fa2581ea3f57",
+    "url": "/static/css/main.02c7d3fb.chunk.css"
+  },
+  {
+    "revision": "46bcdac5a41ed01e1c64",
+    "url": "/static/js/2.594d0bef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.594d0bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe67ba9fa2581ea3f57",
+    "url": "/static/js/main.59bd1d0a.chunk.js"
+  },
+  {
+    "revision": "102e77476dd6a6cc8257",
+    "url": "/static/js/runtime-main.5bea7d8d.js"
+  },
+  {
+    "revision": "d5b3a3f4e8e785bb26ae3d8259dd3228",
+    "url": "/static/media/Rem.d5b3a3f4.png"
+  },
+  {
+    "revision": "2e01eb8129c014d5947ffbb1ef913ec6",
+    "url": "/static/media/Yotsugi.2e01eb81.jpg"
+  }
+]);
